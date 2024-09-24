e7f912e901fb7ad5bb98d52b92e8c7b8ce3745a0
Milad <Milad@gmail.com>
2024-09-24 21:05:35 +0300

    create custom note item

---
+++ lib/widgets/note_item.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top:24,bottom: 24,left: 16),
+      decoration: BoxDecoration(
+        color: Colors.amber,
+        borderRadius: BorderRadiusDirectional.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 12.0, bottom: 4),
+              child: Text(
+                'Build your carrer with mimo',
+                style: TextStyle(color: Colors.black.withOpacity(0.5), fontSize: 20),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(size: 25 , color: Colors.black, Icons.delete),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Text(
+              'data',
+              style: TextStyle(color: Colors.black.withOpacity(0.5),fontSize: 16 ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
